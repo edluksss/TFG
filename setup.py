382f4dd2c4ae28ab084e4c6ed3cfe62382804c32
edluksss <eduardo.lucas.antolin@udc.es>
2024-07-14 14:42:55 +0200

    cambio

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='pnebulae_torch',  # Aquí va el nombre de tu biblioteca
     version='0.1',  # Aquí va la versión de tu biblioteca
-    packages=find_packages() # Encuentra las carpetas con código fuente
+    packages=find_packages(), # Encuentra las carpetas con código fuente
+    package_dir={'': 'src'}
 )
