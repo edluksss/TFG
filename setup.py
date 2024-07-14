cc68178360c36aa8c771f96248d6f0d44554e1a9
edluksss <eduardo.lucas.antolin@udc.es>
2024-07-14 14:34:38 +0200

    actualizacion de la liubreria 2

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name='pnebulae_torch',  # Aquí va el nombre de tu biblioteca
     version='0.1',  # Aquí va la versión de tu biblioteca
-    packages=['src', 'src.cluster'] # Encuentra las carpetas con código fuente
+    packages=find_packages() # Encuentra las carpetas con código fuente
 )
